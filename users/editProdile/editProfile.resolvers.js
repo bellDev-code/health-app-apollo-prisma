4debe071994056b9c9824a5343ffe4703f44e0bc
bellDev-code <sumaoo20@naver.com>
2023-01-26 13:49:34 +0900

    editProfile : devide resolverFn

--- users/editProdile/editProfile.resolvers.js
+++ users/editProdile/editProfile.resolvers.js
@@ -2,42 +2,41 @@ import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { username, email, name, password: newPassword },
+  { loggedInUser }
+) => {
+  let hashPassword = null;
+
+  if (newPassword) {
+    hashPassword = await bcrypt.hash(newPassword, 10);
+  }
+
+  const updateUser = await client.user.update({
+    where: { id: loggedInUser.id },
+    data: {
+      username,
+      email,
+      name,
+      ...(hashPassword && { password: hashPassword }),
+    },
+  });
+
+  if (updateUser) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "프로필을 업데이트 할 수 없습니다.",
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        { username, email, name, password: newPassword },
-        { loggedInUser, protectResolver }
-      ) => {
-        protectResolver(loggedInUser);
-        let hashPassword = null;
-
-        if (newPassword) {
-          hashPassword = await bcrypt.hash(newPassword, 10);
-        }
-
-        const updateUser = await client.user.update({
-          where: { id: loggedInUser.id },
-          data: {
-            username,
-            email,
-            name,
-            ...(hashPassword && { password: hashPassword }),
-          },
-        });
-
-        if (updateUser) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "프로필을 업데이트 할 수 없습니다.",
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
