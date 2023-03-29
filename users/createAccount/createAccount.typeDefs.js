2a97d014c7cb7faecedfb1bd4fb85af70b97d028
bellDev-code <sumaoo20@naver.com>
2023-03-29 18:16:25 +0900

    CreateAccount refactoring

--- users/createAccount/createAccount.typeDefs.js
+++ users/createAccount/createAccount.typeDefs.js
@@ -1,16 +1,12 @@
 import { gql } from "graphql-tag";
 
 export default gql`
-  type CreateAccountResult {
-    ok: Boolean!
-    error: String
-  }
   type Mutation {
     createAccount(
       username: String!
       email: String!
       name: String
       password: String!
-    ): CreateAccountResult!
+    ): MutationResponse!
   }
 `;
