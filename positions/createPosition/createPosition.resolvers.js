a4d63bd4e23496e444638616878b6e648df8799c
bellDev-code <sumaoo20@naver.com>
2023-04-20 18:03:48 +0900

    Mutation CreatePosition

--- positions/createPosition/createPosition.resolvers.js
+++ positions/createPosition/createPosition.resolvers.js
@@ -5,6 +5,20 @@ export default {
   Mutation: {
     createPosition: protectedResolver(
       async (_, { latitude, longitude, gymname }, { loggedInUser }) => {
+        const foundPosition = await client.position.findUnique({
+          where: {
+            userId: loggedInUser.id,
+          },
+        });
+
+        if (foundPosition) {
+          await client.position.delete({
+            where: {
+              id: foundPosition.id,
+            },
+          });
+        }
+
         await client.position.create({
           data: {
             latitude,
